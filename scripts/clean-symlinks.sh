0c17fb817d4d72edbbdbf54c3ae6d0542ff2fbde
ayberkozgur <ayberk.ozgur@epfl.ch>
2014-11-08 21:11:42 +0100

    Updated scripts

--- scripts/clean-symlinks.sh
+++ scripts/clean-symlinks.sh
@@ -6,18 +6,19 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 BASEPATH=$(readlink -f "$SCRIPTPATH/../")
 
 #Clean all symlinks
-cd "$BASEPATH" && find . -type f -not -path "./scripts/*" | while read DOTFILE
+cd "$BASEPATH" && find . -type f -not -path "./scripts/*" -not -path "./.git/*" -not -name ".gitignore" -not -name ".gitmodules" | while read DOTFILE
 do
     SOURCEFILE=$(readlink -f "$BASEPATH/$DOTFILE")
     TARGETFILE="$HOME/dotfiles/.${DOTFILE:2:${#DOTFILE}}" # *************REMOVE dotfiles
     TARGETDIR=$(dirname "$TARGETFILE")
+
     if [ "$(readlink -f "$TARGETFILE")" == "$SOURCEFILE" ]
     then
-        echo $0": Removing target symlink "$TARGETFILE
+        echo "$0: Removing target symlink $TARGETFILE"
         rm "$TARGETFILE"
         rmdir --ignore-fail-on-non-empty -p "$TARGETDIR"
     else
-        echo $0": Not removing target "$TARGETFILE", points to something else, not a symlink or does not exist"
+        echo "$0: Not removing target $TARGETFILE, points to something else, not a symlink or does not exist"
     fi
 done
 
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,17 +1,45 @@
 #!/bin/bash
 
+error_and_die(){
+     >&2 echo $1
+     exit 1
+}
+
 #Get necessary paths
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 BASEPATH=$(readlink -f "$SCRIPTPATH/../")
 
-#List all dotfiles
-cd "$BASEPATH" && find . -type f -not -path "./scripts/*" | while read DOTFILE
+#Get -f argument
+if [ "$1" == "-f" ]
+then
+    LNARG="-f"
+fi
+
+#Install all dotfiles as symlinks
+echo "$0: Installing dotfiles..."
+cd "$BASEPATH" && find . -type f -not -path "./scripts/*" -not -path "./.git/*" -not -name ".gitignore" -not -name ".gitmodules" | while read DOTFILE
 do
     SOURCEFILE=$(readlink -f "$BASEPATH/$DOTFILE")
     TARGETFILE="$HOME/dotfiles/.${DOTFILE:2:${#DOTFILE}}" # *************REMOVE dotfiles
     TARGETDIR=$(dirname "$TARGETFILE")
-    mkdir -p "$TARGETDIR"
-    ln -s "$SOURCEFILE" "$TARGETFILE"
-done
 
+    echo "$0: Installing $SOURCEFILE to $TARGETFILE"
+    mkdir -p "$TARGETDIR"
+    ln -s $LNARG "$SOURCEFILE" "$TARGETFILE" || error_and_die "$0: Installation failed; run as \`$0 -f\` to overwrite target files"
+done
+if [ "$?" -ne "0" ]; then exit 1; fi #The pipe in the loop introduces a subshell so we can't exit the whole script from inside the loop
+echo $0": Installing dotfiles...done."
+
+#Set up our bashrc
+
+
+#Mess with baloo
+echo "$0: Locking baloo out..."
+chmod 555 ~/.local/share/baloo || exit 1
+echo "$0: Locking baloo out...done."
+
+#Update font cache
+echo "$0: Updating font cache..."
+fc-cache -vf ~/.fonts/ || exit 1
+echo "$0: Updating font cache...done."
