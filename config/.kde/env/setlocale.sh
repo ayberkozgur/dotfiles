a0da96a07b614d4b651a2a9b62b14e8d574a6c08
ayberkozgur <ayberk.ozgur@epfl.ch>
2014-11-08 22:04:58 +0100

    Added kde setlocale

---
+++ config/.kde/env/setlocale.sh
@@ -0,0 +1,11 @@
+export LANG=en_CH.UTF-8
+export LANGUAGE=en
+export LC_NUMERIC=en_CH.UTF-8
+export LC_TIME=en_CH.UTF-8
+export LC_MONETARY=en_CH.UTF-8
+export LC_PAPER=en_CH.UTF-8
+export LC_IDENTIFICATION=en_CH.UTF-8
+export LC_NAME=en_CH.UTF-8
+export LC_ADDRESS=en_CH.UTF-8
+export LC_TELEPHONE=en_CH.UTF-8
+export LC_MEASUREMENT=en_CH.UTF-8
